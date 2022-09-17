f9e46d122f141f89117aa91c22d0239651d41836
Peter John <peter.john@pyros.sh>
2022-09-17 19:30:16 +0530

    v0.3.1

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,22 @@ mod text_element;
 
 pub use self::render::Render;
 pub use fragment::Fragment;
+use tide::{http::mime, StatusCode};
 pub use tide_jsx_impl::{component, html, rsx};
 pub use simple_element::SimpleElement;
 pub use text_element::Raw;
 
 impl<'a, T: Render> From<SimpleElement<'a, T>> for tide::Response {
-    fn from(s: SimpleElement<T>) -> Self {
-        tide::Body::from_string(s.render()).into()
+    fn from(s: SimpleElement<'a, T>) -> Self {
+        tide::Response::builder(StatusCode::Ok)
+            .content_type(mime::HTML)
+            .body(s.render())
+            .build()
+    }
+}
+
+impl<'a, T: Render> From<SimpleElement<'a, T>> for tide::Result {
+    fn from(s: SimpleElement<'a, T>) -> Self {
+        Ok(s.into())
     }
 }
