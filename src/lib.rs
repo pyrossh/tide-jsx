38235508b4a99feaede575bd09f929f3e86299a9
Peter John <peter.john@pyros.sh>
2022-09-18 09:18:55 +0530

    remove tide dependency

--- src/lib.rs
+++ src/lib.rs
@@ -10,35 +10,4 @@ pub use self::render::Render;
 pub use fragment::Fragment;
 pub use simple_element::SimpleElement;
 pub use text_element::Raw;
-use tide::{http::mime, StatusCode};
 pub use tide_jsx_impl::{component, html, rsx, view};
-
-impl<'a, T: Render> From<SimpleElement<'a, T>> for tide::Response {
-    fn from(s: SimpleElement<'a, T>) -> Self {
-        tide::Response::builder(StatusCode::Ok)
-            .content_type(mime::HTML)
-            .body(s.render())
-            .build()
-    }
-}
-
-impl<'a, T: Render> From<SimpleElement<'a, T>> for tide::Result {
-    fn from(s: SimpleElement<'a, T>) -> Self {
-        Ok(s.into())
-    }
-}
-
-impl<T: Render> From<Fragment<T>> for tide::Response {
-    fn from(s: Fragment<T>) -> Self {
-        tide::Response::builder(StatusCode::Ok)
-            .content_type(mime::HTML)
-            .body(s.render())
-            .build()
-    }
-}
-
-impl<T: Render> From<Fragment<T>> for tide::Result {
-    fn from(s: Fragment<T>) -> Self {
-        Ok(s.into())
-    }
-}
